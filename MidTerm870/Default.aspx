7ddb318ca31910e339a12d1da1b3a74c4f62b9f8
Evgeniy Petin <34118433+solidmetall@users.noreply.github.com>
2017-12-12 12:40:19 -0800

    Add files via upload

---
+++ MidTerm870/Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="lblName" runat="server" Text="Name:"></asp:Label>
+        <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="lblColor" runat="server" Text="Color:"></asp:Label>
+        <asp:DropDownList ID="ddlColor" runat="server">
+            <asp:ListItem>Green</asp:ListItem>
+            <asp:ListItem>Blue</asp:ListItem>
+            <asp:ListItem>Red</asp:ListItem>
+        </asp:DropDownList>
+&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnRemember" runat="server" onclick="btnRemember_Click" 
+            Text="Remember Me" />
+        <br />
+        <br />
+        <asp:Label ID="lblFavoriteColor" runat="server" Font-Bold="True" 
+            Font-Italic="True" Font-Size="Large"></asp:Label>
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="lblMyColor" runat="server" Text="What was my color?"></asp:Label>
+        <br />
+        <asp:DropDownList ID="ddlMycolor" runat="server" AutoPostBack="True" 
+            onselectedindexchanged="ddlMycolor_SelectedIndexChanged" Width="130px">
+        </asp:DropDownList>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ MidTerm870/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnRemember_Click(object sender, EventArgs e)
+    {
+        if (txtName.Text != "")
+        {
+            if (Session[txtName.Text] != null)
+            {
+                //Session[txtName.Text] = null;
+                Session.Remove(txtName.Text);
+                ddlMycolor.Items.Remove(txtName.Text);
+            }
+            lblFavoriteColor.Text = "Your Favorite Color is: " + ddlColor.SelectedItem.Text;
+            ddlMycolor.Items.Add(txtName.Text);
+            string VA = ddlColor.SelectedItem.Text;
+            string name = txtName.Text;
+            Session.Add(name, VA);
+            txtName.Text = "";
+
+            lblFavoriteColor.ForeColor = System.Drawing.Color.FromName(ddlColor.SelectedItem.Text);
+            //        lblFavoriteColor.ForeColor = System.Drawing.KnownColor.
+        }
+    }
+    protected void ddlMycolor_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        //lblFavoriteColor.Text = "Your Favorite Color is: " + ddlColor.SelectedItem.Text;
+        if (Session[ddlMycolor.SelectedItem.Text] != null)
+        {
+            string VA = (string)Session[ddlMycolor.SelectedItem.Text];
+            lblFavoriteColor.Text = "Your Favorite Color is: " + VA;
+            lblFavoriteColor.ForeColor = System.Drawing.Color.FromName(VA);
+        }
+        //else
+        //{
+        //    lblFavoriteColor.Text = "Your Favorite Color is: " + ddlColor.SelectedItem.Text;
+        //}
+    }
+}
